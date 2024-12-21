c4ac788e342a9367a8c9d377a5beb0424b3a697c
FarhanAli2024 <farhan03062148620@gmail.com>
2024-12-22 02:42:12 +0500

    Add files via upload

---
+++ javascript i/app.js
@@ -0,0 +1,67 @@
+// alert("hello\nfarhan")
+
+//chapter 1
+//q1
+// alert("hello")
+//q2
+//alert("Error:  please enter a valid password")
+//q3
+//alert("welcome to js land \n happy coding")
+//q4
+//alert ("welcome to js land")
+//alert ("happy coding")
+// q5 
+// console.log("hello  i can run js through my web brower's console")
+//q6
+//alert ("my existing project")
+//q7
+// done
+// chapter 2
+// q1
+// console.log("userName = farhan /ali")
+// q2
+// console.log("fullName = farhan ali")
+// q3
+// var message="hello world";
+// alert(message)
+// q4
+// var name = "farhan";
+// var age = "17";
+// var course = "web development "
+// alert (name)
+// alert (age)
+// alert (course)
+// q5
+// var food = "pizza\npizz\npiz\npi\np"
+// alert(food)
+//q6
+// var email= " my email address is farhan03002623167@gmail.com"
+// alret(email)
+// q7 
+// alert("a smarter way to learn javascript ")
+//q8
+// var image = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(image)
+//chapter3
+// q1
+// var age = "17";
+// alert(age)
+//q2
+// var visitor= 15;
+// alert(" you have visited "+visitor+"times")
+// q3
+// var birthYear= 2007;
+// document.write("my birthyear is " + birthYear + ".")
+// q4
+// var visitorName= "Farhan Ali";
+// var Producttitle= "Cloth";
+// var quantity= 7;
+// document.write("Name"+visitorName + "Product" +Producttitle + "Quantity" +quantity)
+
+
+
+
+
+
+
+
